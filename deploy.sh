149261c01daae74bddfc07d32863c46f151fba9f
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2019-02-02 15:40:54 +0100

    Updated stuff

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@ echo "npm install"
 npm install || exit -1
 echo "npm install -g serverless"
 npm install -g serverless || exit -1
+echo "sls create_domain"
+sls create_domain --stage=$1 || exit -1
 echo "sls deploy"
 sls deploy --stage=$1 || exit -1
