b0a02a99273ebf41c6afc8a1d9f8ac44446fb862
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2019-02-02 15:30:32 +0100

    ci wip

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-npm install
-npm install -g serverless
-sls deploy
+echo "npm install"
+npm install || exit -1
+echo "npm install -g serverless"
+npm install -g serverless || exit -1
+echo "sls deploy"
+sls deploy || exit -1
